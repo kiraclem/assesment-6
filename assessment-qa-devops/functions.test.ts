2579e81906b91ccf7744093ab4fec33fd96f882f
Kira Clements <kiwi.klc@gmail.com>
2022-12-16 13:37:20 -0700

    fixed passing in array instead of function

--- assessment-qa-devops/functions.test.ts
+++ assessment-qa-devops/functions.test.ts
@@ -1,11 +1,13 @@
 const {shuffleArray} = require('./utils')
 
+let array = [3, 7, 8, 4, 6]
+
 describe('shuffleArray should', () => {
     test('return an array', () => {
-        expect(Array.isArray(shuffleArray)).toBeTruthy()
+        expect(Array.isArray(shuffleArray(array))).toBeTruthy()
 
     test('length to equal arguments', () => {
-        expect(shuffleArray.length).toEqual(5)
+        expect(shuffleArray(array).length).toEqual(5)
     })
     })
 })
